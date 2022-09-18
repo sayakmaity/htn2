e6ef95bdf8053d9c726b7cd9b4c6e13daeddd748
Sayak Maity <smaity@college.harvard.edu>
2022-09-18 05:24:24 -0400

    wireframe

--- script.js
+++ script.js
@@ -1,60 +1,46 @@
-// const { Client } = require('pg')
-
-// const client = new Client(process.env.DATABASE_URL)
-
-// client.connect()
-
 var first;
 var last;
 var healthCard;
 var postal;
 
 function nextPage() {
-    first = document.getElementById('first').value;
-    last = document.getElementById('last').value;
-    healthCard = document.getElementById('healthCard').value;
-    postal = document.getElementById('postal').value;
-    // console.log(first)
-    // FIND WAY to navigate to next page...
+    // first = document.getElementById('first').value;
+    // last = document.getElementById('last').value;
+    // healthCard = document.getElementById('healthCard').value;
+    // postal = document.getElementById('postal').value;
+
+    // open incident.html
+    window.location.href = "incident.html";
+
+
+    // client.connect(function (err) {
+    //     if (err) throw err;
+    //     console.log("Connected!");
+    //     var sql = "INSERT INTO users.one (id, firstName, lastName, zip) VALUES ('" + healthCard + "', '" + first + "', '" + last + "', '" + postal + "')";
+    //     client.query(sql, function (err, result) {
+    //         if (err) throw err;
+    //         console.log("1 record inserted");
+    //     });
+    // });
 }
 
 
+function nextPage2() {
 
-// take name as javascript input and add it to cockroachdb
-function addName() {
-    var name = document.getElementById("name").value;
-    var query = "INSERT INTO names (name) VALUES ('" + name + "')";
-    client.query(query, (err, res) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log(res.rows[0])
-        }
-    })
+
+    // open ubereats.html
+    window.location.href = "ubereats.html";
+
+    
+
+    // client.connect(function (err) {
+    //     if (err) throw err;
+    //     console.log("Connected!");
+    //     var sql = "INSERT INTO users.one (id, firstName, lastName, zip) VALUES ('" + healthCard + "', '" + first + "', '" + last + "', '" + postal + "')";
+    //     client.query(sql, function (err, result) {
+    //         if (err) throw err;
+    //         console.log("1 record inserted");
+    //     });
+    // });
 }
 
-
-
-
-// var username = document.getElementById("username");
-// var pass = document.getElementById("pass");
-// pass.onfocus = function () {
-//     if (pass.value == "ENTER PASSWORD") {
-//         pass.value = "";
-//     }
-// };
-// username.onfocus = function () {
-//     if (this.value == "ENTER USERNAME") {
-//         this.value = "";
-//     }
-// };
-// pass.onblur = function () {
-//     if (this.value == "") {
-//         pass.value = "ENTER PASSWORD";
-//     }
-// };
-// username.onblur = function () {
-//     if (this.value == "") {
-//         this.value = "ENTER USERNAME";
-//     }
-// };
